fe9395840c6bbd01850c709e43728a7c82417805
Anne Barrios <commits.noreply@annelb.com>
2020-10-20 16:41:22 +0800

    Laid out layout

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,6 +8,14 @@
   <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
 <body>
+  <div class="w-full">
+    <div class="w-3/12 absolute top-4 right-4">
+      <div class="bg-green-100 border border-green-400 text-green-700 px-4 py-2 rounded relative" role="alert">
+        <p class="text-sm">Event saved successfully</p>
+      </div>
+    </div>
+  </div>
+
   <div class="w-11/12 mx-auto my-10">
     <h1 class="heading-1">Calendar</h1>
 
@@ -30,21 +38,28 @@
           </div>
         </div>
 
-        <div class="flex mt-6 inp-checkgroup">
-          <div class="inline-grid grid-cols-4 gap-x-2">
-            <label for="ev-wk-mon"><input type="checkbox" name="" id="ev-wk-mon">Mon</label>
-            <label for="ev-wk-tue"><input type="checkbox" name="" id="ev-wk-tue">Tue</label>
-            <label for="ev-wk-wed"><input type="checkbox" name="" id="ev-wk-wed">Wed</label>
-            <label for="ev-wk-thu"><input type="checkbox" name="" id="ev-wk-thu">Thu</label>
-            <label for="ev-wk-fri"><input type="checkbox" name="" id="ev-wk-fri">Fri</label>
-            <label for="ev-wk-sat"><input type="checkbox" name="" id="ev-wk-sat">Sat</label>
-            <label for="ev-wk-sun"><input type="checkbox" name="" id="ev-wk-sun">Sun</label>
-          </div>
+        <div class="flex flex-wrap mt-6 inp-checkgroup">
+          <label for="ev-wk-mon"><input type="checkbox" name="" id="ev-wk-mon">Mon</label>
+          <label for="ev-wk-tue"><input type="checkbox" name="" id="ev-wk-tue">Tue</label>
+          <label for="ev-wk-wed"><input type="checkbox" name="" id="ev-wk-wed">Wed</label>
+          <label for="ev-wk-thu"><input type="checkbox" name="" id="ev-wk-thu">Thu</label>
+          <label for="ev-wk-fri"><input type="checkbox" name="" id="ev-wk-fri">Fri</label>
+          <label for="ev-wk-sat"><input type="checkbox" name="" id="ev-wk-sat">Sat</label>
+          <label for="ev-wk-sun"><input type="checkbox" name="" id="ev-wk-sun">Sun</label>
+        </div>
+
+        <div class="mt-6">
+          <a href="#" class="btn btn-purple">Save</a>
         </div>
       </div>
 
-      <div class="w-9/12">
-        test
+      <div class="w-9/12 mt-6 pl-10">
+        <h2 class="heading-2 font-bold border-none">October 2020</h2>
+
+        <div class="table w-full table-event">
+          <div class="">1 Mon</div>
+          <div class="">1 Tue</div>
+        </div>
       </div>
     </div>
 
