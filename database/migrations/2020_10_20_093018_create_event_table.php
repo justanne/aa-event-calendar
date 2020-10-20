f2343c0b23eb09118e72549c3926200be4c5f871
Anne Barrios <commits.noreply@annelb.com>
2020-10-20 17:54:09 +0800

    Basic migration

---
+++ database/migrations/2020_10_20_093018_create_event_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEventTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->id();
+            $table->string('event_name');
+            $table->date('event_from');
+            $table->date('event_to');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('events');
+    }
+}
